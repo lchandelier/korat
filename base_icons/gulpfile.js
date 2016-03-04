3f24c49bf97f9ef0062291ae4a487bd28bbf6c4a
Mylène Chandelier <mchandelier@sqli.com>
2016-03-04 14:25:29 +0100

    Fix gulp-filter version. Its last version prevents compass task from running

--- base_icons/gulpfile.js
+++ base_icons/gulpfile.js
@@ -1,28 +1,28 @@
 /* plugins */
 var gulp = require('gulp'),
-	shell = require('gulp-shell'),
-	browsersync  = require('browser-sync'),
-    compass = require('gulp-compass'),
-    minifyCSS = require('gulp-minify-css'),
-	sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    gulpFilter = require('gulp-filter'),
-    merge = require('merge-stream'),
-    zip = require('gulp-zip'),
-    extender = require('gulp-html-extend'),
-    iconfont = require('gulp-iconfont');
+        shell = require('gulp-shell'),
+        browsersync = require('browser-sync'),
+        compass = require('gulp-compass'),
+        minifyCSS = require('gulp-minify-css'),
+        sourcemaps = require('gulp-sourcemaps'),
+        uglify = require('gulp-uglify'),
+        imagemin = require('gulp-imagemin'),
+        rename = require('gulp-rename'),
+        concat = require('gulp-concat'),
+        notify = require('gulp-notify'),
+        cache = require('gulp-cache'),
+        gulpFilter = require('gulp-filter'),
+        merge = require('merge-stream'),
+        zip = require('gulp-zip'),
+        extender = require('gulp-html-extend'),
+        iconfont = require('gulp-iconfont');
 
 /* paths */
 
 var paths = {
     src: 'site/',
     dist: 'dist/',
-	styleguide: 'styleguide/src/assets/toolkit/'
+    styleguide: 'styleguide/src/assets/toolkit/'
 };
 
 var assets = {
@@ -34,7 +34,7 @@ var assets = {
     html: paths.src + 'html',
     icon_font: paths.src + 'icon_font',
     tpl: paths.src + 'tpl',
-	styleguide_css: paths.styleguide + 'styles',
+    styleguide_css: paths.styleguide + 'styles',
     styleguide_js: paths.styleguide + 'scripts'
 };
 
@@ -59,52 +59,52 @@ var urlSync = 'test.local';
 
 /* remove print css from concatenation + Concatenate & Minify CSS */
 gulp.task('compass', function () {
-  var filterPrint = gulpFilter(['*', '!print.scss']);
+    var filterPrint = gulpFilter(['*', '!print.scss']);
 
-  var all = gulp.src([assets.scss + '/screen.scss',
-                      assets.scss + '/**/*.scss',
-                      assets.scss + '/*.scss'])
-                .pipe(filterPrint)
-                .pipe(compass({
-                  config_file: 'config.rb',
-                  css: assets.css,
-                  sass: assets.scss
-                }))
+    var all = gulp.src([assets.scss + '/screen.scss',
+        assets.scss + '/**/*.scss',
+        assets.scss + '/*.scss'])
+            .pipe(filterPrint)
+            .pipe(compass({
+                config_file: 'config.rb',
+                css: assets.css,
+                sass: assets.scss
+            }))
 
-                .pipe(sourcemaps.init())
-                .pipe(concat('toolkit.scss'))
-                .pipe(gulp.dest(assets.styleguide_css))
-                .pipe(notify({message: 'toolkit.scss generated'}))
-                .pipe(rename('all.css'))    
-                .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest(assets.css))
-                .pipe(notify({message: 'all.css generated'}))
-                .pipe(minifyCSS())
-                .pipe(rename({suffix: '.min'}))
-                .pipe(gulp.dest(assets.css))
-				.pipe(browsersync.reload({ stream:true }))
-                .pipe(notify({message: 'all.min.css generated'}));
+            .pipe(sourcemaps.init())
+            .pipe(concat('toolkit.scss'))
+            .pipe(gulp.dest(assets.styleguide_css))
+            .pipe(notify({message: 'toolkit.scss generated'}))
+            .pipe(rename('all.css'))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'all.css generated'}))
+            .pipe(minifyCSS())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(assets.css))
+            .pipe(browsersync.reload({stream: true}))
+            .pipe(notify({message: 'all.min.css generated'}));
 
-  var print = gulp.src(assets.scss + 'print.scss')
-                  .pipe(compass({
-                    config_file: 'config.rb',
-                    css: assets.css,
-                    sass: assets.scss
-                  }))
-                  .pipe(gulp.dest(assets.css))
-                  .pipe(notify({message: 'print.css generated'}))
-                  .pipe(minifyCSS())
-                  .pipe(rename({suffix: '.min'}))
-                  .pipe(gulp.dest(assets.css))
-                  .pipe(notify({message: 'print.min.css generated'}));
+    var print = gulp.src(assets.scss + 'print.scss')
+            .pipe(compass({
+                config_file: 'config.rb',
+                css: assets.css,
+                sass: assets.scss
+            }))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'print.css generated'}))
+            .pipe(minifyCSS())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'print.min.css generated'}));
 
-  return all, print;
+    return all, print;
 });
 
 /* Concatenate & Minify JS */
 gulp.task('scripts', function () {
-  return gulp.src([assets.js + '/src/lib/*.js', assets.js + '/src/*.js']) //manage order
-			.pipe(sourcemaps.init())
+    return gulp.src([assets.js + '/src/lib/*.js', assets.js + '/src/*.js']) //manage order
+            .pipe(sourcemaps.init())
             .pipe(concat('toolkit.js')) //for fabricator
             .pipe(gulp.dest(assets.styleguide_js))
             .pipe(notify({message: 'toolkit.js generated'}))
@@ -112,15 +112,15 @@ gulp.task('scripts', function () {
             .pipe(gulp.dest(assets.js))
             .pipe(rename({suffix: '.min'}))
             .pipe(uglify())
-			.pipe(sourcemaps.write('/'))
+            .pipe(sourcemaps.write('/'))
             .pipe(gulp.dest(assets.js))
-			.pipe(browsersync.reload({ stream:true }))
+            .pipe(browsersync.reload({stream: true}))
             .pipe(notify({message: 'Scripts task complete'}));
 });
 
 /* Optimise images */
 gulp.task('images', function () {
-  return gulp.src(assets.img + '/**/*')
+    return gulp.src(assets.img + '/**/*')
             .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
             .pipe(gulp.dest(assets.img));
 });
@@ -129,63 +129,63 @@ gulp.task('images', function () {
 var runTimestamp = Math.round(Date.now() / 1000);
 gulp.task('iconfont', function () {
     return gulp.src([assets.icon_font + '/*.svg'])
-                .pipe(iconfont({
-                    fontName: myFont,
-                    appendUnicode: true,
-                    normalize: true,
-                    fontHeight: 16,
-                    formats: ['ttf', 'eot', 'woff2'],
-                    timestamp: runTimestamp
-                }))
-                .on('glyphs', function (glyphs, options) {
-                    // CSS templating, e.g.
-                    console.log(glyphs, options);
-                })
-                .pipe(gulp.dest(assets.css+'/fonts/'));
+            .pipe(iconfont({
+                fontName: myFont,
+                appendUnicode: true,
+                normalize: true,
+                fontHeight: 16,
+                formats: ['ttf', 'eot', 'woff2'],
+                timestamp: runTimestamp
+            }))
+            .on('glyphs', function (glyphs, options) {
+                // CSS templating, e.g.
+                console.log(glyphs, options);
+            })
+            .pipe(gulp.dest(assets.css + '/fonts/'));
 });
 
 /* include html patterns in main files */
 gulp.task('extend_common', function () {
-  gulp.src([assets.inc+'/*.html'])
-      .pipe(extender({annotations: false, verbose: false}))
-	  .pipe(browsersync.reload({ stream:true }))
-      .pipe(gulp.dest(assets.inc));
+    gulp.src([assets.inc + '/*.html'])
+            .pipe(extender({annotations: false, verbose: false}))
+            .pipe(browsersync.reload({stream: true}))
+            .pipe(gulp.dest(assets.inc));
 });
 
 gulp.task('extend', function () {
-  gulp.src([assets.html+'/*.html'])
-      .pipe(extender({annotations: false, verbose: false}))
-	  .pipe(browsersync.reload({ stream:true }))
-      .pipe(gulp.dest('.'));
+    gulp.src([assets.html + '/*.html'])
+            .pipe(extender({annotations: false, verbose: false}))
+            .pipe(browsersync.reload({stream: true}))
+            .pipe(gulp.dest('.'));
 });
 
 /* get Fabricator and put it in styleguide folder */
 gulp.task('styleguide', shell.task([
-  'curl -o styleguide/temp.tar.gz -L https://github.com/fbrctr/fabricator/archive/master.tar.gz',
-  'tar -xzvf  styleguide/temp.tar.gz -C styleguide --strip 1',
-  'rm -rf styleguide/temp.tar.gz'
+    'curl -o styleguide/temp.tar.gz -L https://github.com/fbrctr/fabricator/archive/master.tar.gz',
+    'tar -xzvf  styleguide/temp.tar.gz -C styleguide --strip 1',
+    'rm -rf styleguide/temp.tar.gz'
 ]));
 
 /* Create an archive with the current date containing all files needed
  * we exlude unecessary files for the customer like scss and sprite source
  */
 gulp.task('zipDelivery', function () {
-  var date = new Date().toLocaleDateString().replace(/[^0-9]/g, '');
-  return gulp.src(['*.html',
-                    assets.css+'/**',
-                    assets.img+'/**',
-                    assets.js+'/**'], {base: "."})
+    var date = new Date().toLocaleDateString().replace(/[^0-9]/g, '');
+    return gulp.src(['*.html',
+        assets.css + '/**',
+        assets.img + '/**',
+        assets.js + '/**'], {base: "."})
 
-              .pipe(zip('delivery' + date + '.zip'))
-              .pipe(gulp.dest(paths.dist))
-              .pipe(notify({message: 'Archive generated'}));
+            .pipe(zip('delivery' + date + '.zip'))
+            .pipe(gulp.dest(paths.dist))
+            .pipe(notify({message: 'Archive generated'}));
 });
 
 /* browsersync */
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browsersync.init({
-		proxy: urlSync,
-		port: 3000
+        proxy: urlSync,
+        port: 3000
     });
 });
 
@@ -195,10 +195,10 @@ gulp.task('browsersync-reload', function () {
 
 // Watch Files For Changes
 gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch(assets.scss+'/**/*.scss', ['compass']);
-  gulp.watch(assets.js+'/src/**/*.js', ['scripts']);
-  gulp.watch(assets.img+'/**/*', ['images']);
-  gulp.watch([assets.html+'/*.html', assets.inc+'/*.html'], ['extend_common', 'extend']);
+    gulp.watch(assets.scss + '/**/*.scss', ['compass']);
+    gulp.watch(assets.js + '/src/**/*.js', ['scripts']);
+    gulp.watch(assets.img + '/**/*', ['images']);
+    gulp.watch([assets.html + '/*.html', assets.inc + '/*.html'], ['extend_common', 'extend']);
 });
 
 
--- base_sprites/gulpfile.js
+++ base_sprites/gulpfile.js
@@ -1,30 +1,30 @@
 /* plugins */
 var gulp = require('gulp'),
-	shell = require('gulp-shell'),
-	browsersync  = require('browser-sync'),
-    compass = require('gulp-compass'),
-    minifyCSS = require('gulp-minify-css'),
-	sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    gulpFilter = require('gulp-filter'),
-    merge = require('merge-stream'),
-    zip = require('gulp-zip'),
-    extender = require('gulp-html-extend'),
-    svgo = require('imagemin-svgo'),
-    svg2png = require('gulp-svg2png'),
-    svgspritesheet = require('gulp-svg-spritesheet');
+        shell = require('gulp-shell'),
+        browsersync = require('browser-sync'),
+        compass = require('gulp-compass'),
+        minifyCSS = require('gulp-minify-css'),
+        sourcemaps = require('gulp-sourcemaps'),
+        uglify = require('gulp-uglify'),
+        imagemin = require('gulp-imagemin'),
+        rename = require('gulp-rename'),
+        concat = require('gulp-concat'),
+        notify = require('gulp-notify'),
+        cache = require('gulp-cache'),
+        gulpFilter = require('gulp-filter'),
+        merge = require('merge-stream'),
+        zip = require('gulp-zip'),
+        extender = require('gulp-html-extend'),
+        svgo = require('imagemin-svgo'),
+        svg2png = require('gulp-svg2png'),
+        svgspritesheet = require('gulp-svg-spritesheet');
 
 /* paths */
 
 var paths = {
     src: 'site/',
     dist: 'dist/',
-	styleguide: 'styleguide/src/assets/toolkit/'
+    styleguide: 'styleguide/src/assets/toolkit/'
 };
 
 var assets = {
@@ -38,19 +38,19 @@ var assets = {
     html: paths.src + 'html',
     tpl: paths.src + 'tpl',
     css_img_path: '../img',
-	styleguide_css: paths.styleguide + 'styles',
+    styleguide_css: paths.styleguide + 'styles',
     styleguide_js: paths.styleguide + 'scripts'
 };
 
 // folders in sprites directory. Allow you to create 3 different sprites
 var folder = new Array('global',
-                      'home',
-                      'mobile');
+        'home',
+        'mobile');
 
 // url of your project
 var urlSync = 'test.local';
 
-                     
+
 /*
  * Tasks
  * 1/ compass
@@ -66,52 +66,52 @@ var urlSync = 'test.local';
 
 /* remove print css from concatenation + Concatenate & Minify CSS */
 gulp.task('compass', function () {
-  var filterPrint = gulpFilter(['*', '!print.scss']);
+    var filterPrint = gulpFilter(['*', '!print.scss']);
 
-   var all = gulp.src([assets.scss + '/screen.scss',
-                      assets.scss + '/**/*.scss',
-                      assets.scss + '/*.scss'])
-                .pipe(filterPrint)
-                .pipe(compass({
-                  config_file: 'config.rb',
-                  css: assets.css,
-                  sass: assets.scss
-                }))
+    var all = gulp.src([assets.scss + '/screen.scss',
+        assets.scss + '/**/*.scss',
+        assets.scss + '/*.scss'])
+            .pipe(filterPrint)
+            .pipe(compass({
+                config_file: 'config.rb',
+                css: assets.css,
+                sass: assets.scss
+            }))
 
-                .pipe(sourcemaps.init())
-                .pipe(concat('toolkit.scss'))
-                .pipe(gulp.dest(assets.styleguide_css))
-                .pipe(notify({message: 'toolkit.scss generated'}))
-                .pipe(rename('all.css'))    
-                .pipe(sourcemaps.write('./'))
-                .pipe(gulp.dest(assets.css))
-                .pipe(notify({message: 'all.css generated'}))
-                .pipe(minifyCSS())
-                .pipe(rename({suffix: '.min'}))
-                .pipe(gulp.dest(assets.css))
-				.pipe(browsersync.reload({ stream:true }))
-                .pipe(notify({message: 'all.min.css generated'}));
+            .pipe(sourcemaps.init())
+            .pipe(concat('toolkit.scss'))
+            .pipe(gulp.dest(assets.styleguide_css))
+            .pipe(notify({message: 'toolkit.scss generated'}))
+            .pipe(rename('all.css'))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'all.css generated'}))
+            .pipe(minifyCSS())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(assets.css))
+            .pipe(browsersync.reload({stream: true}))
+            .pipe(notify({message: 'all.min.css generated'}));
 
-  var print = gulp.src(assets.scss + 'print.scss')
-                  .pipe(compass({
-                    config_file: 'config.rb',
-                    css: assets.css,
-                    sass: assets.scss
-                  }))
-                  .pipe(gulp.dest(assets.css))
-                  .pipe(notify({message: 'print.css generated'}))
-                  .pipe(minifyCSS())
-                  .pipe(rename({suffix: '.min'}))
-                  .pipe(gulp.dest(assets.css))
-                  .pipe(notify({message: 'print.min.css generated'}));
+    var print = gulp.src(assets.scss + 'print.scss')
+            .pipe(compass({
+                config_file: 'config.rb',
+                css: assets.css,
+                sass: assets.scss
+            }))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'print.css generated'}))
+            .pipe(minifyCSS())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest(assets.css))
+            .pipe(notify({message: 'print.min.css generated'}));
 
-  return all, print;
+    return all, print;
 });
 
 /* Concatenate & Minify JS */
 gulp.task('scripts', function () {
-  return gulp.src([assets.js + '/src/lib/*.js', assets.js + '/src/*.js']) //manage order
-			.pipe(sourcemaps.init())
+    return gulp.src([assets.js + '/src/lib/*.js', assets.js + '/src/*.js']) //manage order
+            .pipe(sourcemaps.init())
             .pipe(concat('toolkit.js')) //for fabricator
             .pipe(gulp.dest(assets.styleguide_js))
             .pipe(notify({message: 'toolkit.js generated'}))
@@ -119,19 +119,18 @@ gulp.task('scripts', function () {
             .pipe(gulp.dest(assets.js))
             .pipe(rename({suffix: '.min'}))
             .pipe(uglify())
-			.pipe(sourcemaps.write('/'))
+            .pipe(sourcemaps.write('/'))
             .pipe(gulp.dest(assets.js))
-			.pipe(browsersync.reload({ stream:true }))
+            .pipe(browsersync.reload({stream: true}))
             .pipe(notify({message: 'Scripts task complete'}));
 });
 
 /* Optimise images */
 gulp.task('images', function () {
-  return gulp.src(assets.img + '/**/*')
+    return gulp.src(assets.img + '/**/*')
             .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
             .pipe(gulp.dest(assets.img));
 });
-
 
 /* sprites management 
  * note : pixelbase option = font-size on body in your CSS file
@@ -139,72 +138,72 @@ gulp.task('images', function () {
 
 gulp.task('sprites', function () {
 
-  folder.forEach(function (name) {
-    gulp.src(assets.sprites + '/'+ name + '/*.svg')
-        .pipe(svgspritesheet({
-          cssPathNoSvg: assets.css_img_path + '/sprite_' + name + '.png',
-          cssPathSvg: assets.css_img_path + '/sprite_' + name + '.svg',
-          padding: 10,
-          positioning: 'diagonal',
-          templateDest: assets.util + '/_sprite_' + name + '.scss',
-          templateSrc: assets.tpl + '/sprite-template-' + name + '.tpl',
-          pixelBase: 14,
-          units: 'px',
-          x: 0,
-          y: 0
-        }))
-        .pipe(svgo({
-          plugins: [
-            {removeXMLProcInst: false}
-          ]
-        })())
-        .pipe(gulp.dest(assets.img + '/sprite_' + name + '.svg'))
-        .pipe(svg2png())
-        .pipe(gulp.dest(assets.img + '/sprite_' + name + '.png'))
-        .pipe(notify({message: 'Sprite ' + name + ' generated'}));
-  });
+    folder.forEach(function (name) {
+        gulp.src(assets.sprites + '/' + name + '/*.svg')
+                .pipe(svgspritesheet({
+                    cssPathNoSvg: assets.css_img_path + '/sprite_' + name + '.png',
+                    cssPathSvg: assets.css_img_path + '/sprite_' + name + '.svg',
+                    padding: 10,
+                    positioning: 'diagonal',
+                    templateDest: assets.util + '/_sprite_' + name + '.scss',
+                    templateSrc: assets.tpl + '/sprite-template-' + name + '.tpl',
+                    pixelBase: 14,
+                    units: 'px',
+                    x: 0,
+                    y: 0
+                }))
+                .pipe(svgo({
+                    plugins: [
+                        {removeXMLProcInst: false}
+                    ]
+                })())
+                .pipe(gulp.dest(assets.img + '/sprite_' + name + '.svg'))
+                .pipe(svg2png())
+                .pipe(gulp.dest(assets.img + '/sprite_' + name + '.png'))
+                .pipe(notify({message: 'Sprite ' + name + ' generated'}));
+    });
 });
 
 /* include html patterns in main files */
 gulp.task('extend_common', function () {
-  gulp.src([assets.inc+'/*.html'])
-      .pipe(extender({annotations: false, verbose: false}))
-      .pipe(gulp.dest(assets.inc));
+    gulp.src([assets.inc + '/*.html'])
+            .pipe(extender({annotations: false, verbose: false}))
+            .pipe(gulp.dest(assets.inc));
 });
 
 gulp.task('extend', function () {
-  gulp.src([assets.html+'/*.html'])
-      .pipe(extender({annotations: false, verbose: false}))
-      .pipe(gulp.dest('.'));
+    gulp.src([assets.html + '/*.html'])
+            .pipe(extender({annotations: false, verbose: false}))
+            .pipe(gulp.dest('.'));
 });
 
 /* get Fabricator and put it in styleguide folder */
 gulp.task('styleguide', shell.task([
-  'curl -o styleguide/temp.tar.gz -L https://github.com/fbrctr/fabricator/archive/master.tar.gz',
-  'tar -xzvf  styleguide/temp.tar.gz -C styleguide --strip 1',
-  'rm -rf styleguide/temp.tar.gz'
+    'curl -o styleguide/temp.tar.gz -L https://github.com/fbrctr/fabricator/archive/master.tar.gz',
+    'tar -xzvf  styleguide/temp.tar.gz -C styleguide --strip 1',
+    'rm -rf styleguide/temp.tar.gz'
 ]));
 
 /* Create an archive with the current date containing all files needed
  * we exlude unecessary files for the customer like scss and sprite source
  */
 gulp.task('zipDelivery', function () {
-  var date = new Date().toLocaleDateString().replace(/[^0-9]/g, '');
-  return gulp.src(['*.html',
-                    assets.css+'/**',
-                    assets.img+'/**',
-                    assets.js+'/**'], {base: "."})
+    var date = new Date().toLocaleDateString().replace(/[^0-9]/g, '');
+    return gulp.src(['*.html',
+        assets.css + '/**',
+        assets.img + '/**',
+        assets.js + '/**'], {base: "."})
 
-              .pipe(zip('delivery' + date + '.zip'))
-              .pipe(gulp.dest(paths.dist))
-              .pipe(notify({message: 'Archive generated'}));
+        .pipe(zip('delivery' + date + '.zip'))
+        .pipe(gulp.dest(paths.dist))
+        .pipe(notify({message: 'Archive generated'}));
 });
 
 /* browsersync */
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browsersync.init({
-		proxy: urlSync,
-		port: 3000
+        proxy: urlSync,
+        port: 3000
     });
 });
 
@@ -213,12 +212,12 @@ gulp.task('browsersync-reload', function () {
 });
 
 // Watch Files For Changes
-gulp.task('watch', ['browser-sync'],  function () {
-  gulp.watch(assets.scss+'/**/*.scss', ['compass']);
-  gulp.watch(assets.js+'/src/**/*.js', ['scripts']);
-  gulp.watch(assets.img+'/**/*', ['images']);
-  gulp.watch(assets.sprites+'/**', ['sprites']);
-  gulp.watch([assets.html+'/*.html', assets.inc+'/*.html'], ['extend_common', 'extend']);
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch(assets.scss + '/**/*.scss', ['compass']);
+    gulp.watch(assets.js + '/src/**/*.js', ['scripts']);
+    gulp.watch(assets.img + '/**/*', ['images']);
+    gulp.watch(assets.sprites + '/**', ['sprites']);
+    gulp.watch([assets.html + '/*.html', assets.inc + '/*.html'], ['extend_common', 'extend']);
 });
 
 
